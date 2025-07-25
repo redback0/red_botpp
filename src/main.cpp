9b3a6bec584f48a915c4d7a57a108df166d1237a
redback0 <nate@thejackos.net>
2025-07-26 09:08:47 +0930

    Added todo and yo wuddup

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,35 @@
 #include <dpp/dpp.h>
 #include <cstdlib>
 #include <iostream>
+#include <string>
 
-int main(int ac, char** av, char** ev)
+std::string get_bot_token()
 {
-    const char* token = std::getenv("BOT_TOKEN");
+    std::ifstream env(".env");
 
-    if (!token)
+    std::string line;
+
+    while (std::getline(env, line).good())
+    {
+        if (line.starts_with("BOT_TOKEN="))
+        {
+            return line.substr(std::strlen("BOT_TOKEN="));
+        }
+    }
+    return std::string();
+}
+
+int main(int ac, char** av)
+{
+    std::string token = get_bot_token();
+
+    if (token.empty())
     {
         std::cout << "Failed to get token" << std::endl;
         exit(1);
     }
 
-    dpp::cluster bot(token);
+    dpp::cluster bot(token, dpp::i_default_intents | dpp::i_message_content);
 
     bot.on_log(dpp::utility::cout_logger());
 
@@ -23,6 +40,15 @@ int main(int ac, char** av, char** ev)
         if (event.command.get_command_name() == "ping")
         {
             event.reply("Pong!");
+        }
+    });
+
+    bot.on_message_create([&bot](const dpp::message_create_t& event)
+    {
+        if (event.msg.author.is_bot()) return;
+        if (event.msg.content == "yo wuddup")
+        {
+            event.send("yo wuddup");
         }
     });
 
