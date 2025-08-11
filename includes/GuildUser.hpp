270dfcef0b645a9d0a1650d70d457f92ab5c6230
redback0 <gh@ttpg.xyz>
2025-08-12 05:41:48 +0930

    eco daily complete

--- includes/GuildUser.hpp
+++ includes/GuildUser.hpp
@@ -1,4 +1,5 @@
 
+#include <chrono>
 #include <mutex>
 #include <map>
 #include <random>
@@ -6,10 +7,16 @@
 #include <utility>
 #include "botDatabase.h"
 
+using namespace std::literals;
+
 class GuildUser
 {
 public:
     typedef sqlite3_int64 wallet_int;
+
+    typedef std::chrono::seconds gu_time_t;
+
+    typedef std::chrono::time_point<std::chrono::system_clock> gu_tp_t;
 
     typedef std::string lock_map_key_t;
     typedef std::map<lock_map_key_t, std::mutex> lock_map_t;
@@ -22,8 +29,8 @@ private:
     std::string _user_id;
     wallet_int   _wallet;
     wallet_int   _bank;
-    //  _last_daily;
-    //  _last_steal;
+    gu_time_t _last_daily;
+    gu_time_t _last_steal;
     // later, inv
 
     static std::mutex _map_lock;
@@ -33,6 +40,8 @@ private:
 
     const static wallet_int DEFAULT_WALLET = 2000ll;
     const static wallet_int DEFAULT_BANK = 0ll;
+    constexpr const static gu_time_t DEFAULT_LAST_DAILY = 0s;
+    constexpr const static gu_time_t DEFUALT_LAST_STEAL = 0s;
 
     const static wallet_int DAILY_AMOUNT = 500ll;
 
